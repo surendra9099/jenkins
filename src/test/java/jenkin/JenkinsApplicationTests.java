d7aeca206d0cbe2eb055b519317a6a78d9a263c8
surendra sinhg rajawat <surendra.rajawat@atcs.com>
2020-08-17 20:12:20 +0530

    add new project

---
+++ src/test/java/jenkin/JenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package jenkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
