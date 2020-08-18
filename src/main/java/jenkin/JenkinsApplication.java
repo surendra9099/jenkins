3d386229e8435113630243c11fd9030e4539f435
surendra sinhg rajawat <surendra.rajawat@atcs.com>
2020-08-18 12:38:06 +0530

    changes in pom

--- src/main/java/jenkin/JenkinsApplication.java
+++ src/main/java/jenkin/JenkinsApplication.java
@@ -2,9 +2,10 @@ package jenkin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsApplication {
+public class JenkinsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsApplication.class, args);
